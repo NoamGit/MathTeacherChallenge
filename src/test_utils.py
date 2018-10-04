52dbc7439b6bbc81be71b05b10e3789cb24c93ef
NoamGit <noambox@gmail.com>
2018-10-04 14:45:53 +0300

    advanced solve equation

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-
-from utils import solve_eq_string
+import pandas as pd
+from utils import solve_eq_string, is_number
 
 
 class TestUtils(TestCase):
@@ -8,4 +8,18 @@ class TestUtils(TestCase):
     def test_solve_eq_string(self):
         eq = ["unkn: x,y", "equ: x + 3 = y", "equ: 2*y + 12 = 5*x"]
         res = solve_eq_string(eq)
-        self.assertEqual(res,(6,9))
+        self.assertEqual(res,[6,9])
+
+    def test_solve_eq_all(self):
+        train = pd.read_json('../Data/dolphin-number_word_std/number_word_std.dev.json')
+        test = pd.read_json('../Data/dolphin-number_word_std/number_word_std.test.json')
+
+        # train["equations"].apply(lambda x:set(solve_eq_string(x)))
+        # train["ans"].apply(.iloc[6]
+
+        # bad ii = 4
+        ii = 344
+        problem = test.iloc[ii]
+        res = solve_eq_string(problem["equations"],integer_flag=is_number(problem["text"]))
+        # if isinstance(res,List[list])
+        self.assertAlmostEqual(list(set(res)), list(set(problem["ans_simple"])))
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,26 @@
 from typing import List
 
 from sympy.parsing.sympy_parser import parse_expr
-from sympy import symbols
-from sympy.solvers import solve
+from sympy import symbols,var,Symbol
+from sympy import solveset, S
+from sympy.sets.sets import EmptySet
 from sympy.solvers.solveset import linsolve
+from sympy.solvers import solve
 
+IS_NUM_DICT = ['integer','consecutive',]
 # region Utilities
-def solve_eq_string(math_eq_format: List[str]):
+
+def switch_sign(txt:str):
+    minus_pos = txt.find('-')
+    plus_pos = txt.find('+')
+    txt_list = list(txt)
+    if minus_pos != -1:
+        txt_list[minus_pos] = '+'
+    if plus_pos != -1:
+        txt_list[plus_pos] = '-'
+    return ''.join(txt_list)
+
+def solve_eq_string(math_eq_format: List[str], integer_flag=False):
     """
     >>  ["unkn: x,y",
     >>  "equ: x + 3 = y",
@@ -15,25 +29,63 @@ def solve_eq_string(math_eq_format: List[str]):
     :return:
     """
     var_str = math_eq_format[0].split('unkn: ')[-1]
+    sym_var = tuple()
+    # TODO: fix hack of constraining integer solution
+    for v in var_str.split(','):
+        var = Symbol(v)
+        # var = Symbol(v, integer=integer_flag) if integer_flag else Symbol(v)
+        sym_var+=(var,)
+
     eq_list = []
+    parse_eq_list = []
     for eq in math_eq_format[1:]:
 
         # remove whitespaces and move to onesided equation
         rhs,lhs = eq.split('equ: ')[-1].replace(' ','').split('=')
-        if len(lhs) > len(rhs):
-            lhs,rhs=rhs,lhs
+        parse_eq_list += [parse_expr(lhs, evaluate=True) * -1 + parse_expr(rhs, evaluate=True)]
 
-        # oneside handling with negation
-        if (lhs[:2] =='-(' and lhs[-1] == ')') or (lhs[0] == '-'):
-            lhs[0] = '+'
+    from sympy import nsolve
+    sol = solve(parse_eq_list, sym_var)
+    if isinstance(sol,dict):
+        eval_sol = [parse_expr(v).evalf(subs=dict(sol)) for v in var_str.split(',')]
+        if sol == []:
+            return []
+        elif integer_flag and not all([y.is_integer for x, y in sol.items()]):
+            return []
         else:
-            lhs = '-' + lhs
+            return eval_sol
+    elif isinstance(sol,list):
+        eval_sol = []
+        for s in sol:
+            sol_dict = dict(zip([s.strip() for s in var_str.split(',')],s))
+            eval_itr = [parse_expr(v).evalf(subs=sol_dict) for v in var_str.split(',')]
+            if sol == []:
+                continue
+            elif integer_flag and not all([y.is_integer for x,y in sol_dict.items() if y.is_number]):
+                continue
+            else:
+                eval_sol += [eval_itr]
+        return eval_sol
 
-        eq_str = rhs+lhs
-        eq_list += [parse_expr(eq_str, evaluate=True)]
+    # print(solve(parse_eq_list, sym_var))
+    # print(solve(parse_eq_list, (x,y)))
+    # print(solve([-x + y - 4, 2*x - 5*y + 11], sym_var))
+    # print(solve([-x + y - 4, 2*x - 5*y + 11], (x,y)))
 
-    sol = linsolve(eq_list, tuple(symbols(var_str)))
-    return list(sol)[0]
 
-    # extracts symbols from
+def is_number(query_text:str):
+    query_text = query_text.lower()
+    if any([t in query_text for t in IS_NUM_DICT]):
+        return True
+    else:
+        return False
+
+def parse_ans_col(ans: str):
+    if ans == 'ans_no_result':
+        return []
+    else:
+        ans.replace
+        if '|' not in ans:
+            pass
+
 # endregion
